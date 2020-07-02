5e1d3539267aed54a49757e9eec525990053c240
Edmund Yu <38992135+Nolelle@users.noreply.github.com>
2020-07-01 18:03:16 -0600

    working

---
+++ js/script.js
@@ -0,0 +1,17 @@
+ //Put first field in the 'focus' state 
+ document.getElementById("name").focus();
+ 
+ //Add an Other option to the Job Role section 
+
+ function showOrHideTip(show, element) {
+     if (show) {
+         element.style.display = "inherit";
+     } else {
+         element.style.display = "none";
+     }
+ }
+
+
+
+//T-Shirt Section 
+//Activity Section 
