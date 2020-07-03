9a14ff3b659817982b86c26d7e776dade0c82d96
Edmund Yu <38992135+Nolelle@users.noreply.github.com>
2020-07-03 17:24:02 -0600

    working

--- js/script.js
+++ js/script.js
@@ -19,4 +19,23 @@
 
  
 //T-Shirt Section 
+//On first load of form, Color should display Please select a t shirt theme, colors in drop down should be hidden. 
+var  color = document.getElementById("color");
+var cornflowerblue = color.options[1];
+var darkslategrey = color.options[2];
+var gold = color.options[3];
+var tomato = color.options[4];
+var steelblue = color.options[5];
+var dimgrey = color.options[6];
+
+cornflowerblue.hidden = true;
+darkslategrey.hidden = true;
+gold.hidden = true;
+tomato.hidden = true;
+steelblue.hidden = true;
+dimgrey.hidden = true;
+
+// If a theme is chosen enable color choices that are appropriate to the chosen theme.
+
+
 //Activity Section 
